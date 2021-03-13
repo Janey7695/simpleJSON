3b22962c7892ada845b60247042366cb64cc7ce8
janey <1737941060@qq.com>
2021-03-13 21:45:33 +0800

    add parse_number function

--- simpleJson.c
+++ simpleJson.c
@@ -1,5 +1,6 @@
 #include "simpleJson.h"
-
+#include <errno.h>   /* errno, ERANGE */
+#include <math.h>    /* HUGE_VAL */
 #if DEBUG
 #include <assert.h>  /* assert() */
 #endif
@@ -10,13 +11,16 @@
 #define EXPECT(c,ch)    do { assert(*c->json == (ch)); c->json++; } while(0)
 #endif
 
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
+
 typedef struct 
 {
     char *json;
 }smon_context;
 
 
-static void smon_prase_whitespace(smon_context* c)
+static void smon_PARSE_whitespace(smon_context* c)
 {
     char *p = c->json;
     while(*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
@@ -33,7 +37,7 @@ false = "false"
 true  = "true"
 */
 
-static int smon_prase_literal(smon_context* c,smon_value* v,char* whichType,int whichPraseType)
+static int smon_PARSE_literal(smon_context* c,smon_value* v,char* whichType,int whichPARSEType)
 {
     size_t i;
 
@@ -49,24 +53,63 @@ static int smon_prase_literal(smon_context* c,smon_value* v,char* whichType,int 
         if (c->json[i] != whichType[i + 1])
             return SMON_PARSE_INVALID_VALUE;
     c->json += i;
-    v->type = whichPraseType;
-    return SMON_PRASE_OK;
+    v->type = whichPARSEType;
+    return SMON_PARSE_OK;
 }
 
+/*
+number = [ "-" ] int [ frac ] [ exp ]
+int = "0" / digit1-9 *digit
+frac = "." 1*digit
+exp = ("e" / "E") ["-" / "+"] 1*digit
+*/
 
-static int smon_prase_value(smon_context* c,smon_value* v)
+static int smon_PARSE_number(smon_context* c,smon_value* v)
+{
+    char *p = c->json;
+    if(*p == '-') p++;
+    if(*p == '0') p++;
+    else
+    {
+        if(!ISDIGIT(*p)) return SMON_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    if(*p == '.')
+    {
+        p++;
+        if(!ISDIGIT(*p)) return SMON_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    if(*p == 'e' || *p == 'E')
+    {
+        p++;
+        if(*p == '+' || *p == '-') p++;
+        if(!ISDIGIT(*p)) return SMON_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    errno = 0;
+    v->n = strtod(c->json,NULL);
+    if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+        return SMON_PARSE_NUMBER_TOO_BIG;
+    v->type =SMON_NUMBER;
+    c->json = p;
+    return SMON_PARSE_OK;
+}
+
+static int smon_PARSE_value(smon_context* c,smon_value* v)
 {
     switch (*c->json)
     {
-    case 't':return smon_prase_literal(c,v,"true",SMON_TRUE);
-    case 'f':return smon_prase_literal(c,v,"false",SMON_FALSE);
-    case 'n':return smon_prase_literal(c,v,"null",SMON_NULL);
-    default: return SMON_PARSE_INVALID_VALUE;
+    case 't':return smon_PARSE_literal(c,v,"true",SMON_TRUE);
+    case 'f':return smon_PARSE_literal(c,v,"false",SMON_FALSE);
+    case 'n':return smon_PARSE_literal(c,v,"null",SMON_NULL);
+    case '\0':return SMON_PARSE_NOT_VALUE;
+    default: return smon_PARSE_number(c,v);
     }
 }
 
 
-int smon_prase(smon_value* v, char* json)
+int smon_PARSE(smon_value* v, char* json)
 {
     smon_context c;
     int ret;
@@ -77,10 +120,10 @@ int smon_prase(smon_value* v, char* json)
     #endif
 
     v->type = SMON_NULL;
-    smon_prase_whitespace(&c);
-    if((ret = smon_prase_value(&c,v)) == SMON_PRASE_OK)
+    smon_PARSE_whitespace(&c);
+    if((ret = smon_PARSE_value(&c,v)) == SMON_PARSE_OK)
     {
-        smon_prase_whitespace(&c);
+        smon_PARSE_whitespace(&c);
         if(*c.json != '\0')
             ret = SMON_PARSE_ROOT_NOT_SINGULAR;
     }
@@ -94,4 +137,13 @@ smon_type smon_get_type(smon_value* v)
     #endif
 
     return v->type;
+}
+
+double smon_get_number(smon_value* v)
+{
+    #if DEBUG
+    assert(v != NULL && v->type ==SMON_NUMBER);
+    #endif
+
+    return v->n;
 }
--- simpleJson.h
+++ simpleJson.h
@@ -7,22 +7,26 @@ typedef enum {
     SMON_NULL = 0,
     SMON_TRUE,
     SMON_FALSE,
+    SMON_NUMBER
 }smon_type;
 
 enum {
-    SMON_PRASE_OK = 0, //successful prasing return SMON_PRASE_OK
+    SMON_PARSE_OK = 0, //successful prasing return SMON_PARSE_OK
     SMON_PARSE_NOT_VALUE, //if not give value(a null json) will return SMON_PARSE_NOT_VALUE
     SMON_PARSE_INVALID_VALUE, // if give a invalid value will return SMON_PARSE_INVALID_VALUE
-    SMON_PARSE_ROOT_NOT_SINGULAR // If there are other characters after the value, an error `SMON_PARSE_ROOT_NOT_SINGULAR` is returned  
+    SMON_PARSE_ROOT_NOT_SINGULAR, // If there are other characters after the value, an error `SMON_PARSE_ROOT_NOT_SINGULAR` is returned  
+    SMON_PARSE_NUMBER_TOO_BIG,
 };
 
 typedef struct
 {
+    double n;
     smon_type type;
 }smon_value;
 
 
-int smon_prase(smon_value* v, char* json);
-smon_type smon_get_type(smon_value* v);
+int smon_PARSE(smon_value* v, char* json);// PARSE function
+smon_type smon_get_type(smon_value* v); // Get the type of a value
+double smon_get_number(smon_value* v);// Get the number of a value
 
 #endif // !SIMPLEJSON_H__
